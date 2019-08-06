294abc99e240e35c12e32e2145d7c872df90c507
onurceldir123 <onurmrtcldrmail.com>
2019-08-06 22:13:57 +0300

    Commit My Launcher

--- app/src/main/java/com/example/mylauncher/MainActivity.java
+++ app/src/main/java/com/example/mylauncher/MainActivity.java
@@ -34,5 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+        /**test*/
     }
 }
